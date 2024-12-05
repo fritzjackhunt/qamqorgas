9221ac75b37580d30212c84a7f6bdff7cc7797ac
Aniekan Essiet <essiet.aniekan@yahoo.com>
2024-12-05 15:27:37 +0300

    added site

---
+++ qamqor/gas/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'qamqor/index.html')
+
+def about(request):
+    return render(request, 'qamqor/about.html')
+
+def appraisals(request):
+    return render(request, 'qamqor/appraisals.html')
+
+def brokerage(request):
+    return render(request, 'qamqor/brokerage.html')
+
+def consulting(request):
+    return render(request, 'qamqor/consulting.html')
+
+def contact(request):
+    if request.method == "POST":
+        contact_fname = request.POST['fname']
+        contact_lname = request.POST['lname']
+        contact_email = request.POST['email']
+        contact_message = request.POST['message']
+
+        send_mail(
+            contact_fname, 
+            contact_message,
+            contact_email,
+            ['essiet.aniekan@yahoo.com'],
+            fail_silently = False,
+        )
+
+
+        return render(request, 'qamqor/contact.html', {'contact_fname' : contact_fname})
+
+
+    else:
+        return render(request, 'qamqor/contact.html')
